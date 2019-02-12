5375e4dc8e806a34eb2f5c781fba2ef083c9204a
Andreas Plewnia <acplewnia@gmail.com>
2019-02-12 22:14:01 +0100

    Added abstract testable class

---
+++ src/model/testable.ts
@@ -0,0 +1,13 @@
+import { TestResult } from "./test-result/test-result";
+
+export class Testable {
+  constructor(private testResults: TestResult[]) {}
+
+  protected isTested(): boolean {
+    return this.testResults.length > 0;
+  }
+
+  protected isReplayable(): boolean {
+    return this.testResults[this.testResults.length - 1].isReplayable();
+  }
+}
