690dd8547bb316e17a8e01f46d5f2a19bdd1d536
aplewnia <andreas.plewnia@materna.de>
2019-03-25 16:10:04 +0100

    Fixed XpathLocator ClassName

--- src/model/locator/xpath-locator.ts
+++ src/model/locator/xpath-locator.ts
@@ -5,7 +5,7 @@ import { Locator } from './locator';
 
 export class XpathLocator extends Locator {
   constructor(methodName: string, value: string) {
-    super('XPathLocator', methodName, value);
+    super('XpathLocator', methodName, value);
   }
 
   public async test(driver: WebDriver): Promise<Status> {
