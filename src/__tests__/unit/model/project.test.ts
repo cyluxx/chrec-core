4a31af68485558532305cad6e28179aa88f32451
Andreas Plewnia <andreas.plewnia@materna.de>
2019-10-23 16:11:20 +0200

    #34 Add some project tests

---
+++ src/__tests__/unit/model/project.test.ts
@@ -0,0 +1,31 @@
+import { Project } from '../../../model/project';
+import { Sequence } from '../../../model/sequence';
+import { ProjectTestResult } from '../../../model/test-result/project-test-result';
+
+describe('Project', () => {
+  describe('addSequence', () => {
+    test('when Sequence s, then return list of Sequences with s appended to the end', () => {
+      const project = new Project('foo', [new Sequence('bar', [])], []);
+      project.addSequence(new Sequence('s', []));
+
+      expect(project.sequences).toEqual(
+        [new Sequence('bar', []), new Sequence('s', [])]
+      );
+    })
+  })
+});
+
+describe('Project', () => {
+  describe('addTestResult', () => {
+    test('when ProjectTestResult p, then return list of ProjectTestResults with p appended to the end', () => {
+      const date = new Date();
+      const project = new Project('foo', [], [new ProjectTestResult(date, [])]);
+      const dateP = new Date();
+      project.addTestResult(new ProjectTestResult(dateP, []));
+
+      expect(project.projectTestResults).toEqual(
+        [new ProjectTestResult(date, []), new ProjectTestResult(dateP, [])]
+      );
+    })
+  })
+});
