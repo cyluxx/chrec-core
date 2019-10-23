f572499f677e4fc8e378368431d38764aa9e86fe
Andreas Plewnia <andreas.plewnia@materna.de>
2019-10-23 15:45:22 +0200

    #34 Add boundingBox test

---
+++ src/__tests__/unit/model/bounding-box.test.ts
@@ -0,0 +1,10 @@
+import { BoundingBox } from '../../../model/bounding-box';
+
+describe('BoundingBox', () => {
+  describe('fromDOMRect', () => {
+    test('when valid DOMRect, then return valid BoundingBox', () => {
+      const boundingBox = BoundingBox.fromDOMRect({x: 1, y: 2, width: 3, height: 4} as DOMRect);
+      expect(boundingBox).toEqual(new BoundingBox(1, 2, 3, 4));
+    })
+  })
+});
