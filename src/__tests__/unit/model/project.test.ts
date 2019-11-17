f36cf01d021113a30b72be9a098afe7f558e8592
Andreas Plewnia <acplewnia@gmail.com>
2019-11-17 02:32:19 +0100

    Add Project test

--- src/__tests__/unit/model/project.test.ts
+++ src/__tests__/unit/model/project.test.ts
@@ -1,5 +1,7 @@
+import { instance, mock, verify } from 'ts-mockito';
 import { Project } from '../../../model/project';
 import { Sequence } from '../../../model/sequence';
+import { Settings } from '../../../model/settings';
 
 describe('Project', () => {
   describe('addSequence', () => {
@@ -10,6 +12,22 @@ describe('Project', () => {
       expect(project.sequences).toEqual(
         [new Sequence('bar', []), new Sequence('s', [])]
       );
+    })
+  })
+
+  describe('test', () => {
+    test('verify squences are tested', async () => {
+      const mockedSettings: Settings = mock(Settings);
+      const settings = instance(mockedSettings);
+
+      const mockedSequence: Sequence = mock(Sequence);
+      const sequence = instance(mockedSequence);
+
+      const project = new Project('foo', [sequence]);
+
+      project.test(settings);
+
+      verify(mockedSequence.test(settings)).called();
     })
   })
 });
